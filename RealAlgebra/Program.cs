2d9e4a141a518ff3ed7256f53be51306effc597e
suriyaprakash2512 <suriyaprakash.r@mphasis.com>
2023-11-23 15:14:56 +0530

    first commit

---
+++ RealAlgebra/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ShapAreaLib.Area;
+
+namespace RealAlgebra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a shape: 1. Rectangle, 2. Square, 3. Circle, 4. Triangle");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter length: ");
+                    double length = double.Parse(Console.ReadLine());
+                    Console.Write("Enter width: ");
+                    double width = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"Area of Rectangle: {new Rectangle().Area(length, width)}");
+                    break;
+
+                case 2:
+                    Console.Write("Enter side length: ");
+                    double side = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"Area of Square: {new Square().Area(side)}");
+                    break;
+
+                case 3:
+                    Console.Write("Enter radius: ");
+                    double radius = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"Area of Circle: {new Circle().Area(radius)}");
+                    break;
+
+                case 4:
+                    Console.Write("Enter base length: ");
+                    double baseLength = double.Parse(Console.ReadLine());
+                    Console.Write("Enter height: ");
+                    double height = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"Area of Triangle: {new Triangle().Area(baseLength, height)}");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ShapAreaLib/Area.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapAreaLib
+{
+    public class Area
+    {
+
+        public class Rectangle
+        {
+            public double Area(double length, double width)
+            {
+                return length * width;
+            }
+        }
+
+        public class Square
+        {
+            public double Area(double side)
+            {
+                return side * side;
+            }
+        }
+
+        public class Circle
+        {
+            public double Area(double radius)
+            {
+                return Math.PI * radius * radius;
+            }
+        }
+
+        public class Triangle
+        {
+            public double Area(double baseLength, double height)
+            {
+                return 0.5 * baseLength * height;
+            }
+        }
+
+    }
+}
